144e6b0597b7cf176ecf633c201e1c290fe89679
Amir Massarwi <amassarwi@gmail.com>
2017-05-09 22:40:45 +0200

    change app component location and delete index component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 import {CommonModule} from '@angular/common';
 
 import {AppComponent}  from './app.component';
-import {IndexComponent} from './index/index.component';
 
 @NgModule({
     imports:        [BrowserModule, CommonModule],
-    declarations:   [AppComponent, IndexComponent],
+    declarations:   [AppComponent],
     bootstrap:      [AppComponent]
 })
 
