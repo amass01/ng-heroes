6c51448ca846349cf20178877788fabd4b89792c
Amir Massarwi <amassarwi@gmail.com>
2017-05-09 23:55:33 +0200

    Added hero object and updated the app component to show the hero details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,11 @@
 import {NgModule}      from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {CommonModule} from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import {AppComponent}  from './app.component';
 
 @NgModule({
-    imports:        [BrowserModule, CommonModule],
+    imports:        [BrowserModule, FormsModule],
     declarations:   [AppComponent],
     bootstrap:      [AppComponent]
 })
