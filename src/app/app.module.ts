ef16c4922c6ab7e0f901f00217f3c41a400411eb
Amir Massarwi <amassarwi@gmail.com>
2017-05-10 21:48:52 +0200

    Add heroes component and make app component a container shell

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,21 @@
-import {NgModule}      from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 
-import {AppComponent}  from './app.component';
+import { AppComponent }  from './app.component';
+import { HeroesComponent }  from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
 
 @NgModule({
     imports:        [BrowserModule, FormsModule],
     declarations:   [
         AppComponent,
+        HeroesComponent,
         HeroDetailComponent
+    ],
+    providers: [
+        HeroService
     ],
     bootstrap:      [AppComponent]
 })
